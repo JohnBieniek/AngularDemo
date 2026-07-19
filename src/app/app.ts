1db362fbc32f5486d5782bec58f142ebacd0c596
John Bieniek <1126076+JohnBieniek@users.noreply.github.com>
2026-07-19 17:09:53 -0400

    Display package version in shared footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SiteHeader } from "./shared/site-header/site-header";
+import packageInfo from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { SiteHeader } from "./shared/site-header/site-header";
 })
 export class App {
   protected readonly title = signal('Johns Portfolio');
+  protected readonly version = packageInfo.version;
 }
